88a202b09d8e73fc32dd5a3b28f46fcae779d463
develzy <ppdslirboyo@gmai.com>
2025-12-27 23:41:57 +0700

    chore: remove unused folders (layanan-*) and legacy directory

--- legacy/public/js/modules/pengaturan.js
+++
@@ -1,2 +0,0 @@
-import { UI } from '../core/ui.js';
-export const PengaturanModule = { async init() { const rows = await UI.loadTableData('pengaturan'); this.render(rows); }, render(rows) { /* Generic Render */ } };
