996da6900600bce84036300f6ddfe49e3d5339a2
develzy <ppdslirboyo@gmai.com>
2025-12-30 00:08:05 +0700

    fix(ustadz): add missing useMemo import

--- src/app/sekretariat/ustadz/page.js
+++ src/app/sekretariat/ustadz/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { usePagePermission } from '@/lib/AuthContext';
 import { useToast } from '@/lib/ToastContext';
 import { useDataManagement } from '@/hooks/useDataManagement';
